cd7c57d51dfc4364a3069de4fcc3f0718ea6f6e4
Guram Dzneladze <129051734+Usernamenotavailable12@users.noreply.github.com>
2025-03-04 19:39:53 +0400

    Update maskUsername.js

--- src/maskUsername.js
+++ src/maskUsername.js
@@ -1,5 +1,5 @@
-function maskUsername(username, userId) {
+function maskUsername(username, currentUserId, userId) {
     if (!username || username.length <= 2) return username;
-    if (userId && userId === extractAuthDataFromCookie()?.userId) return username;
+    if (userId && userId === currentUserId) return username;
     return username[0] + '*****' + username[username.length - 1];
 }
