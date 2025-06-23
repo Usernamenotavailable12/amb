47e574ade19380ce6c9896bd8a0f448013157e80
Guram Dzneladze <129051734+Usernamenotavailable12@users.noreply.github.com>
2025-06-23 16:06:10 +0400

    Update userBoxConnection-app.js

--- src/userBoxConnection-app.js
+++ src/userBoxConnection-app.js
@@ -9,7 +9,7 @@ async function fetchActiveUserBoxes() {
 
   const query = `
     query UserBoxConnection($userId: ID) {
-      userBoxConnection(userId: $userId, status: ACTIVE, last: 30) {
+      userBoxConnection(userId: $userId, status: ACTIVE, first: 30) {
         edges {
           node {
             userBoxId
